c08d94d0c0907577e49e9e877b208e7b5afae3d9
Junoxy <junjun.privated@gmail.com>
2024-04-30 15:37:04 +0200

    added check for if dashboard has connnected to Api with conditional rendering for components dependent on ApiData

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,49 +1,55 @@
-import { Route,Routes } from "react-router-dom";
-import { Box } from "@mui/material";
-import Home from "../../pages/Home";
-import SideNav from "../sideNav/SideNav";
-import Playlist from "../../pages/Playlist";
-import { getAccessTokenFromStorage } from "../../utils/getAccessTokenFromStorage";
-import { useEffect, useState } from "react";
-import Player from "../Player/Player";
-import MobileNav from "../MobileNav/MobileNav";
-import Library from '../../pages/Library'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from '../../pages/Home';
+import SideNav from '../sideNav/SideNav';
+import Playlist from '../../pages/Playlist';
+import { getAccessTokenFromStorage } from '../../utils/getAccessTokenFromStorage';
+import { useEffect, useState } from 'react';
+import Player from '../Player/Player';
+import MobileNav from '../MobileNav/MobileNav';
+import Library from '../../pages/Library';
 
-const Dashboard = ( {spotifyAPI } ) => {
-    const [token, setToken] = useState(getAccessTokenFromStorage())
-    
+const Dashboard = ({ spotifyAPI }) => {
+	const [token, setToken] = useState(getAccessTokenFromStorage());
+	const [hasConnected, setHasConnected] = useState(false);
 
-    useEffect(() => {
-        const onMount = async () => {
-            await spotifyAPI.setAccessToken(token)
-        }
-        if(token) {
-            onMount()
-        }
-    }, [])
+	useEffect(() => {
+		const onMount = async () => {
+			await spotifyAPI.setAccessToken(token);
+			setHasConnected(true);
+		};
+		if (token) {
+			onMount();
+		}
+	}, []);
 
-    return <Box sx={{
-        display:'flex',
-        height: '100%',
-        width: '100%',
-        flexDirection: 'column'
-    }}>
-                <Box sx={{
-                    flex: 1,
-                    overflowY: 'auto',
-                    display: 'flex'
-                }}>
-                    <SideNav spotifyAPI={spotifyAPI} token={token}/>
-                    <Routes>
-                        <Route path="/playlist/:id" element={<Playlist spotifyAPI={spotifyAPI} token={token}/>}/>
-                        <Route path="/library" element={<Library spotifyAPI={spotifyAPI} token={token}/>}/>
-                        <Route path='/' element={<Home/>}/>
-                    </Routes>
-                </Box>
-                {token && <Player spotifyAPI={spotifyAPI} token={token} />}
-                <MobileNav/>
-           </Box>
-    
-}
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				height: '100%',
+				width: '100%',
+				flexDirection: 'column'
+			}}
+		>
+			<Box
+				sx={{
+					flex: 1,
+					overflowY: 'auto',
+					display: 'flex'
+				}}
+			>
+				<SideNav spotifyAPI={spotifyAPI} token={token} />
+				<Routes>
+					<Route path="/playlist/:id" element={hasConnected && <Playlist spotifyAPI={spotifyAPI} token={token} />} />
+					<Route path="/library" element={hasConnected && <Library spotifyAPI={spotifyAPI} token={token} />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Box>
+			{token && hasConnected && <Player spotifyAPI={spotifyAPI} token={token} />}
+			<MobileNav />
+		</Box>
+	);
+};
 
 export default Dashboard;
